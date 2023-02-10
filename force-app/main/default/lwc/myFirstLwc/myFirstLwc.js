52bcbe97c8ef0e1363e1a277ffd5606167c4ce1f
Ramasamy Palanivel <ramsdevops@hotmail.com>
2023-02-10 09:32:07 +0530

    comment is added in js file

--- force-app/main/default/lwc/myFirstLwc/myFirstLwc.js
+++ force-app/main/default/lwc/myFirstLwc/myFirstLwc.js
@@ -1,3 +1,15 @@
+/**
+ * @description       : 
+ * @author            : Ramasamy Palanivel
+ * @group             : 
+ * @last modified on  : 02-10-2023
+ * @last modified by  : Ramasamy Palanivel 
+ * Modifications Log
+ * Ver   Date         Author               Modification
+ * 1.0   02-10-2023   Ramasamy Palanivel   Initial Version
+**/
 import { LightningElement } from 'lwc';
 
-export default class MyFirstLwc extends LightningElement {}
+export default class MyFirstLwc extends LightningElement {
+    
+}
